8fbf19ad529d70ebe127be04380b42e98c6bec6a
すりガラス眼鏡 <138665025+megane123321@users.noreply.github.com>
2025-07-11 12:55:17 +0900

    ビューの動的変更のテスト

--- app/src/main/java/com/knct_ci4_2025/kumasuta/MainActivity.java
+++ app/src/main/java/com/knct_ci4_2025/kumasuta/MainActivity.java
@@ -2,6 +2,9 @@ package com.knct_ci4_2025.kumasuta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.knct_ci4_2025.kumasuta.stamp.StampDrawActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static int STAMP_CARD_WID=3;
+    final static int STAMP_CARD_HEI=5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        startActivity(new Intent(this.getBaseContext(),StampDrawActivity.class));
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        setContentView(R.layout.activity_stamp_card);
+        View view=findViewById(R.id.stamp_home);
+        TableRow stamp_table=(TableRow)findViewById(R.id.stamp_card_0);
+        stamp_table.addView(new Button(this.getBaseContext()));
+        setContentView(view);
     }
 }
--- app/src/main/java/com/knct_ci4_2025/kumasuta/stamp/StampDrawActivity.java
+++ app/src/main/java/com/knct_ci4_2025/kumasuta/stamp/StampDrawActivity.java
@@ -2,16 +2,23 @@ package com.knct_ci4_2025.kumasuta.stamp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.knct_ci4_2025.kumasuta.*;
 
 public class StampDrawActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle bundle){
-        super.onCreate(bundle);
-        setContentView(R.layout.view_test);
-        View view=findViewById(R.id.customViewTest);
-        setContentView(view);
-    }
+//    final static int STAMP_CARD_WID=3;
+//    final static int STAMP_CARD_HEI=5;
+//
+//    @Override
+//    protected void onCreate(Bundle bundle){
+//        super.onCreate(bundle);
+//        setContentView(R.layout.activity_stamp_card);
+//        View view=findViewById(R.id.stamp_home);
+//        TableRow stamp_table=(TableRow)findViewById(R.id.stamp_card_0);
+//        stamp_table.addView(new Button(this.getBaseContext()));
+//        setContentView(view);
+//    }
 }
